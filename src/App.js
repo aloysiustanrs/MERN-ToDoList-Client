3235e62a7542bccbf268b92ddeb582e0e55dd3ef
aloysiustanrs <aloysiustanrs@gmail.com>
2022-05-07 03:08:34 +0800

    test commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+const axios = require("axios").default;
 
 function App() {
+  const [listOfItems, setListOfItems] = useState([]);
+
+  const [task, setTask] = useState(""); //for onchange of task
+
+  const addItem = () => {
+    axios
+      .post("https://mern-to-dolist.herokuapp.com/add", { task: task })
+      .then((response) => {
+        setListOfItems([...listOfItems, response.data]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const deleteItem = (id) => {
+    axios
+      .delete(`https://mern-to-dolist.herokuapp.com/delete/${id}`)
+      .then(() => {
+        setListOfItems(
+          listOfItems.filter((item) => {
+            return item._id !== id;
+          })
+        );
+      });
+  };
+
+  useEffect(() => {
+    axios.get("https://mern-to-dolist.herokuapp.com/read").then((response) => {
+      setListOfItems(response.data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div>
+          <input
+            type="text"
+            placeholder="Task"
+            onChange={(event) => {
+              setTask(event.target.value);
+            }}
+          ></input>
+          <button onClick={addItem}>Add Task</button>
+          <hr />
+          {listOfItems.map((item) => {
+            return (
+              <div className="ItemBox">
+                <div className="Item">{item.task}</div>
+                <button
+                  onClick={() => {
+                    deleteItem(item._id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
